296760531e24f62c01af5b128ea4e39bca1926f7
Ustym Buchko <92269527+Ustymko@users.noreply.github.com>
2024-01-02 18:26:38 +0200

    created post, author and tag models for db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.text import slugify
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from datetime import date
 
-# Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
+    email_address = models.EmailField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=40)
+    excerpt = models.CharField(max_length=100)
+    image = models.CharField(max_length=50)
+    date = models.DateField(editable=False)
+    slug = models.CharField(max_length=50, unique=True)
+    text = models.TextField(validators=[MinLengthValidator(10), MaxLengthValidator(2000)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        if not self.id:
+            self.date = date.today()
+        return super(Post, self).save(*args, **kwargs)
